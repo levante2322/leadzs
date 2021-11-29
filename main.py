57821806456d523a1a11724c4891fcaff40be94b
levante2322 <aminyazarloo1376@yahoo.com>
2021-11-28 22:36:34 -0800

    add base

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+    return "arezooo khenge arezooo khenge"
+
+if __name__ == "__main__":
+    import os 
+    PORT = os.getenv("PORT",5000)
+    app.run("0.0.0.0",PORT)
